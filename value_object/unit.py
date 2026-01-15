2d28d1af010d4be0b26e6ac19c0684ccbd076395
themanprince <princeadigwe29@gmail.com>
2026-01-16 00:39:59 +0100

    units

---
+++ value_object/unit.py
@@ -0,0 +1,14 @@
+from uuid import uuid4
+from enum import Enum
+
+
+class Unit(Enum):
+	BSKT = uuid4()
+	BAG = uuid4()
+	KG = uuid4()
+	CTN = uuid4()
+	PCS = uuid4()
+	BTL = uuid4()
+	PKT = uuid4()
+	TUBER = uuid4()
+	BUNCH = uuid4()
